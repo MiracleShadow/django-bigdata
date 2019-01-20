2707a25e81ef9ae97af9b38deb3acbeabfa917b7
MiracleShadow <1210768621@qq.com>
2019-01-21 05:34:40 +0800

    Update urls.py

--- front/urls.py
+++ front/urls.py
@@ -10,5 +10,14 @@ urlpatterns = [
     # 给url取个名字'name=xxx'，以后使用url的时候就使用它的名字进行反转就可以了reverse
     # 若多个app中出现同名url，使用应用命名空间可解决冲突问题
     path('', views.index, name='index'),
-    path('signin/', views.login, name='login')
+    path('if_test/', views.if_test, name='if_test'),
+    path('for_test/', views.for_test, name='for_test'),
+    path('book/', views.book, name='book'),
+    path('book/detail/<book_id>/<category>', views.book_detail, name='book_detail'),
+    path('movie/', views.movie, name='movie'),
+    path('city', views.city, name='city'),
+    path('signin/', views.login, name='login'),
+    path('add/', views.add_view),
+    path('cut/', views.cut_view),
+    path('date/', views.date_view),
 ]
