5a2905d5be33bbf4a502d4c01208fefdb9a2f2a6
MiracleShadow <1210768621@qq.com>
2019-01-25 22:43:03 +0800

    append INSTALLED_APPS
    close CSRF

--- myFirstDjangoProject/settings.py
+++ myFirstDjangoProject/settings.py
@@ -38,14 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'front',
-    'cms'
+    'cms',
+    'QRcode'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
