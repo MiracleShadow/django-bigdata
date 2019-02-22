d6444e48fe869412b2c91c53cedc442d03695fac
MiracleShadow <1210768621@qq.com>
2019-02-22 23:55:18 +0800

    :airplane: Update login, logout, register Views

--- front/views.py
+++ front/views.py
@@ -10,6 +10,9 @@ from .models import Film
 from .models import Tag
 from .models import Region
 from .models import Upload
+from .models import User
+from .forms import UserForm
+from .forms import RegisterForm
 import pandas as pd
 import requests
 import random
@@ -177,25 +180,107 @@ class Weather(object):
 
 
 def index(request):
-    # ?username=xxx
-    connection_mysql_db(request)
-    username = request.GET.get('username')
-    context = {
-        'username': username,
-        "today": datetime.now()
-    }
-    if username:
-        # html = render_to_string("base.html")
-        # return HttpResponse(html)
-        return render(request, 'index.html', context=context)
-    else:
-        # 多个app中出现同名url，使用应用命名空间解决冲突问题
-        login_url = reverse('front:login')
-        return redirect(login_url)
+    # if not request.session.get('is_login', None):
+    #     # 多个app中出现同名url，使用应用命名空间解决冲突问题
+    #     login_url = reverse('front:login')
+    #     return redirect(login_url)
+    return render(request, 'index.html')
 
 
 def login(request):
-    return render(request, 'signin.html')
+    # 不允许重复登录
+    if request.session.get('is_login', None):
+        return redirect(reverse('front:index'))
+
+    if request.method == "POST":
+        login_form = UserForm(request.POST)
+        message = "请检查填写的内容，所有字段都必须填写！"
+        # 使用表单类自带的is_valid()方法一步完成数据验证工作
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            # 验证成功后可以从表单对象的cleaned_data数据字典中获取表单的具体值
+            print('验证成功')
+            if username and password:  # 确保邮箱和密码都不为空
+                username = username.strip()
+                # 用户名字符合法性验证
+                # 密码长度验证
+                # 更多的其它验证.....
+                try:
+                    user = User.objects.get(username=username)
+                    if user.password == password:
+                        print("登陆成功！跳转主页……")
+                        request.session['is_login'] = True
+                        request.session['user_id'] = user.id
+                        request.session['user_name'] = user.username
+                        return redirect(reverse('front:index'))
+                    else:
+                        message = "密码不正确！"
+                except Exception as e:
+                    print(e)
+                    message = "用户名不存在！"
+        return render(request, 'signin.html', locals())
+        # 如果验证不通过，则返回一个包含先前数据的表单给前端页面，方便用户修改。
+        # 也就是说，它会帮你保留先前填写的数据内容，而不是返回一个空表！
+    login_form = UserForm()
+    return render(request, 'signin.html', locals())
+    # 对于非POST方法发送数据时，比如GET方法请求页面，返回空的表单，让用户可以填入数据
+
+
+'''
+Python内置了一个locals()函数，它返回当前所有的本地变量字典，
+我们可以偷懒的将这作为render函数的数据字典参数值，就不用费劲去构造一个形如{'message':message, 'login_form':login_form}的字典了。
+这样做的好处当然是大大方便了我们，但是同时也可能往模板传入了一些多余的变量数据，造成数据冗余降低效率。
+'''
+
+
+def logout(request):
+    if not request.session.get('is_login', None):
+        # 如果本来就未登录，也就没有登出一说
+        return redirect(reverse('front:index'))
+    request.session.flush()
+    # 或者使用下面的方法
+    # del request.session['is_login']
+    # del request.session['user_id']
+    # del request.session['user_name']
+    return redirect(reverse('front:index'))
+
+
+def register(request):
+    if request.session.get('is_login', None):
+        # 登录状态不允许注册。你可以修改这条原则！
+        return redirect("/index/")
+    if request.method == "POST":
+        register_form = RegisterForm(request.POST)
+        message = "请检查填写的内容！"
+        if register_form.is_valid():  # 获取数据
+            username = register_form.cleaned_data['username']
+            password1 = register_form.cleaned_data['password1']
+            password2 = register_form.cleaned_data['password2']
+            email = register_form.cleaned_data['email']
+            if password1 != password2:  # 判断两次密码是否相同
+                message = "两次输入的密码不同！"
+                return render(request, 'register.html', locals())
+            else:
+                same_name_user = User.objects.filter(username=username)
+                if same_name_user:  # 用户名唯一
+                    message = '用户已经存在，请重新选择用户名！'
+                    return render(request, 'register.html', locals())
+                same_email_user = User.objects.filter(email=email)
+                if same_email_user:  # 邮箱地址唯一
+                    message = '该邮箱地址已被注册，请使用别的邮箱！'
+                    return render(request, 'register.html', locals())
+
+                # 当一切都OK的情况下，创建新用户
+
+                new_user = User()
+                new_user.username = username
+                new_user.password = password1
+                new_user.email = email
+                new_user.save()
+                return redirect('/signin/')  # 自动跳转到登录页面
+    register_form = RegisterForm()
+    return render(request, 'register.html', locals())
 
 
 def get_cursor():
