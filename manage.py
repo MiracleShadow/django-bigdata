e708fe7a0ee8b23b109a2ff0ae702f4e9e47980c
MiracleShadow <1210768621@qq.com>
2019-05-28 21:34:18 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myFirstDjangoProject.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bigdata.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
