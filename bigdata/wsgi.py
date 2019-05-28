6b9e59883e2b83b479f407f24d7213c39c04b975
MiracleShadow <1210768621@qq.com>
2019-05-28 21:34:05 +0800

    rename

--- bigdata/wsgi.py
+++ bigdata/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for myFirstDjangoProject project.
+WSGI config for bigdata project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myFirstDjangoProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bigdata.settings')
 
 application = get_wsgi_application()
